6aad12fdb15d97282a3e63858625c4796eee73b4
Divyansh <18698div@gmail.com>
2019-11-13 18:32:41 +0530

    new project added

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.container-1').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
