9dc0369caccd7f3c69794c0bf4ff2f5a64d4f256
Divyansh <18698div@gmail.com>
2019-11-15 18:44:27 +0530

    again commit

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
         arrows: false
     });
+
+    // $('.posts').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     arrows: false
+    //   });
   });
