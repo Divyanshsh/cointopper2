2564e4ecc47c4a6f41943357508e8bd7c9dbca03
Divyansh <18698div@gmail.com>
2019-11-18 18:43:58 +0530

    modified

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 775,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 680,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+              }
+        ]
     });
   });
+
+  function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
